1037bc1f9673266b7473329afadbf12e728257be
Luke Berry <lukeberrypi@gmail.com>
2022-10-28 14:57:25 -0300

    Improve timeConversion solution

--- timeConversion.js
+++ timeConversion.js
@@ -1,15 +1,16 @@
 const EXERCISE_LINK = "https://www.hackerrank.com/challenges/time-conversion/problem";
 // segure CTRL e clique no link para abrir esse desafio no seu navegador
 
-const sampleInput = "07:05:45PM";
+const sampleInput = "11:05:45AM";
 
 function timeConversion(s) {
   const timeCode = s.slice(-2);
   const [hours, minutes, seconds] = s.slice(0, -2).split(":");
   
-  if (timeCode === "AM") return `${hours}:${minutes}:${seconds}$`;
-  if (timeCode === "PM") return `${Number(hours) + 12}:${minutes}:${seconds}`;
+  if (timeCode === "AM") return `${hours}:${minutes}:${seconds}`;
+  if (timeCode === "PM" && hours === "12") return `00:${minutes}:${seconds}`;
 
-}
+  return `${Number(hours) + 12}:${minutes}:${seconds}`
+};
 
 console.log(timeConversion(sampleInput));
