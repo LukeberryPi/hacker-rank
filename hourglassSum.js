a9faaa9d6d0b31c65153328c1631f605c86f4fb7
Luke Berry <luke.berry@newfold.com>
2022-11-07 20:55:42 -0300

    Add solution to hourglassSum exercise

--- hourglassSum.js
+++ hourglassSum.js
@@ -10,30 +10,38 @@ const sampleInput = [
   [0, 0, 1, 2, 4, 0],
 ];
 
-// first sum you have to make
-// is from this central node (1, 1) of value 1:
+// the first sum you have to make
+// is from this central node with coordinates (1, 1) of value 1:
 
-// 1 1 1                                            (0, 0) (0, 1) (0, 2)
+// 1 1 1                                            (0, 0) (1, 0) (2, 0)
 //   1     which can be translated to coordinates          (1, 1)
-// 1 1 1                                            (2, 0) (2, 1) (2, 2)
+// 1 1 1                                            (0, 2) (1, 2) (2, 2)
 
-// in reality, this is the sum of all neighbor nodes minus right and left node.
+// this should result in sum 7
+
+// be mindful that, in math, the coordinates
+// are given as (x, y) analog to (horizontal, vertical) orientation
+// when iterating through a 2D array with arr[i][j]
+//   i stands for which list you're selecting (vertically/up down)
+//   j stands for which number on a list you're selecting (horizontally/left right)
 
 function hourglassSum(arr) {
 
-  const sumSpecificHourglass = (centralNodeX, centralNodeY) => {
-    let resultingSum = 0;
-    
-    resultingSum += Math.sum(arr[centralNodeX - 1][centralNodeY - 1], )
-
-    return resultingSum;
+  const singleHourglassSum = (xCoordinate, yCoordinate) => {
+    return arr[yCoordinate - 1][xCoordinate - 1]
+        + arr[yCoordinate - 1][xCoordinate]
+        + arr[yCoordinate - 1][xCoordinate + 1]
+        + arr[yCoordinate][xCoordinate]
+        + arr[yCoordinate + 1][xCoordinate - 1]
+        + arr[yCoordinate + 1][xCoordinate]
+        + arr[yCoordinate + 1][xCoordinate + 1];
   }
-
+    
   const hourglassSumsArr = [];
-  
+
   for (let i = 1; i < arr.length - 1; i++) {
     for (let j = 1; j < arr.length - 1; j++) {
-      hourglassSumsArr.push(sumSpecificHourglass(i, j));
+      hourglassSumsArr.push(singleHourglassSum(i, j));
     }
   }
   
