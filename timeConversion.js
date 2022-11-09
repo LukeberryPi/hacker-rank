99712c4b4db53cb21cf050249f2bb9c02e58689e
Luke Berry <luke.berry@newfold.com>
2022-11-09 19:37:31 -0300

    Fix implementation for timeConversion exercise

--- timeConversion.js
+++ timeConversion.js
@@ -1,16 +1,16 @@
 const EXERCISE_LINK = "https://www.hackerrank.com/challenges/time-conversion/problem?isFullScreen=true";
 // segure CTRL e clique no link para abrir esse desafio no seu navegador
 
-const sampleInput = "11:05:45AM";
+const sampleInput = "12:45:54PM";
 
 function timeConversion(s) {
   const timeCode = s.slice(-2);
   const [hours, minutes, seconds] = s.slice(0, -2).split(":");
-  
-  if (timeCode === "AM") return `${hours}:${minutes}:${seconds}`;
-  if (timeCode === "PM" && hours === "12") return `00:${minutes}:${seconds}`;
 
-  return `${Number(hours) + 12}:${minutes}:${seconds}`
+  if (timeCode === "AM" && hours === "12") return `00:${minutes}:${seconds}`;
+  if (timeCode === "PM" && hours === "12") return `${hours}:${minutes}:${seconds}`;
+  if (timeCode === "AM") return `${hours}:${minutes}:${seconds}`;
+  if (timeCode === "PM") return `${Number(hours) + 12}:${minutes}:${seconds}`;
 };
 
 console.log(timeConversion(sampleInput));
